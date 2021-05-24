6515f5a24c7be597803a8767ad2f643957f8fc11
charliemerrigan <ckm41686@uga.edu>
2021-05-23 21:57:15 -0400

    Barebones design

--- pizza-app/src/components/Header.js
+++ pizza-app/src/components/Header.js
@@ -1,34 +1,27 @@
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Header = () => {
-    return (
-        <div>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                    <Nav>
-                        <Nav.Link href="#deets">More deets</Nav.Link>
-                        <Nav.Link eventKey={2} href="#memes">
-                            Dank memes
-      </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Navbar
+        className="nv"
+        fixed="top"
+        collapseOnSelect
+        expand="lg"
+        variant="dark"
+      >
+        <Navbar.Brand href="#home">Harry's Pizza and Subs</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#menu">Menu</Nav.Link>
+            <Nav.Link href="#about-us">About Us</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
---
+++ pizza-app/src/components/Homepage.js
@@ -0,0 +1,12 @@
+import pizzaPhoto from "../HarrysLogo.jpeg";
+import { Image } from "react-bootstrap";
+
+const Homepage = () => {
+  return (
+    <div className="home">
+      <h1 class="mainTitle">Harry's Pizza and Subs</h1>
+    </div>
+  );
+};
+
+export default Homepage;
