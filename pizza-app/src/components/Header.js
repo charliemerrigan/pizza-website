ccc831d24c596fa2f98d07684c967040908f4fd0
AidanMulaokar <aidanpm123@gmail.com>
2021-05-27 13:27:28 -0400

    menu almost finished and router added

--- pizza-app/src/components/Header.js
+++ pizza-app/src/components/Header.js
@@ -1,27 +1,29 @@
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Image } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "../images/AidanPizzaMan.png"
 
 const Header = () => {
-  return (
-    <div>
-      <Navbar
-        className="nv"
-        fixed="top"
-        collapseOnSelect
-        expand="lg"
-        variant="dark"
-      >
-        <Navbar.Brand href="#home">Harry's Pizza and Subs</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#menu">Menu</Nav.Link>
-            <Nav.Link href="#about-us">About Us</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
-  );
+    return (
+        <div>
+            <Navbar
+                className="nv"
+                fixed="top"
+                collapseOnSelect
+                expand="lg"
+                variant="dark"
+            >
+                <Image src={logo}></Image>
+                <Navbar.Brand href="/home">Harry's Pizza and Subs</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/menu">Menu</Nav.Link>
+                        <Nav.Link href="/about-us">About Us</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    );
 };
 
 export default Header;
